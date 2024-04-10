cfec2857813cf5e62f446ebd8fff5342edc09a89
NathanBangwa243 <nathanbangwa243@gmail.com>
2024-04-10 23:59:09 +0200

    initialize new module

---
+++ addons/estate/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Real Estate",
+
+    'summary': """
+        Manage your real estate properties with ease.""",
+
+    'description': """
+        This module allows you to efficiently manage your real estate properties, 
+        including properties for sale or rent, potential clients, lease contracts, etc.
+    """,
+
+    'author': "@NathanBangwa243",
+    'website': "http://www.gofamille.org",
+
+    'category': 'Sales/Real Estate',
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
