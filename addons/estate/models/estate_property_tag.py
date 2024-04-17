d992e2e3c751d48c53936f09032b6a43a804da7a
NathanBangwa243 <nathanbangwa243@gmail.com>
2024-04-17 13:08:03 +0200

    filter estate.property.offer by name order

--- addons/estate/models/estate_property_tag.py
+++ addons/estate/models/estate_property_tag.py
@@ -7,6 +7,8 @@ from odoo import fields, models
 class EstatePropertyTag(models.Model):
     _name = 'estate.property.tag'
     _description = "Properties Tag"
+    _order = "name"
+
 
     name = fields.Char(string="Name", required=True, help="Tag name")
 
