ca0ec518aeee8f56914974ecb8d0fdedda68da6b
NathanBangwa243 <nathanbangwa243@gmail.com>
2024-04-18 23:32:22 +0200

    Add the third step of invoice creation

--- addons/estate_account/models/estate_property.py
+++ addons/estate_account/models/estate_property.py
@@ -8,6 +8,8 @@ from odoo import api
 from odoo import exceptions
 from odoo.exceptions import ValidationError
 
+from odoo import Command
+
 class EstateProperty(models.Model):
     _inherit = "estate.property"
 
@@ -21,11 +23,37 @@ class EstateProperty(models.Model):
         # Retrieve the partner_id from the current estate.property
         partner_id = self.buyer_id or False
 
+        # Calculate the amounts for the invoice lines
+        property_name = self.name
+
+        selling_price = self.selling_price
+        administrative_fee = 100.00
+
+        line1_amount = selling_price * 0.06  # 6% of selling price
+        line2_amount = administrative_fee
+
         # Create the account.move with the specified values
         move_values = {
             'partner_id': partner_id.id if partner_id else False,
             'move_type': 'out_invoice',  # Corresponds to 'Customer Invoice'
-            # Add other required fields here
+            "line_ids": [
+                Command.create({
+                    'name': property_name,
+                    "quantity": 1,
+                    "price_unit": selling_price,
+                }),
+                Command.create({
+                    'name': 'Property Sale - 6% of Selling Price',
+                    "quantity": 1,
+                    "price_unit": line1_amount,
+                }),
+                Command.create({
+                    'name': 'Administrative Fees',
+                    "quantity": 1,
+                    "price_unit": line2_amount,
+                }),
+
+            ],
         }
         self.env['account.move'].create(move_values)
 
