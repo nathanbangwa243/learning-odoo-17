4d6b5548795bed9d01db1e2b7b16ee9cec379049
NathanBangwa243 <nathanbangwa243@gmail.com>
2024-04-18 22:52:53 +0200

    Add the second step of invoice creation

--- addons/estate_account/models/estate_property.py
+++ addons/estate_account/models/estate_property.py
@@ -15,5 +15,18 @@ class EstateProperty(models.Model):
         """
 
         """
+        # Call the super method to perform the default action_sold_property logic
+        result = super().action_sold_property()
 
-        return super().action_sold_property()
+        # Retrieve the partner_id from the current estate.property
+        partner_id = self.buyer_id or False
+
+        # Create the account.move with the specified values
+        move_values = {
+            'partner_id': partner_id.id if partner_id else False,
+            'move_type': 'out_invoice',  # Corresponds to 'Customer Invoice'
+            # Add other required fields here
+        }
+        self.env['account.move'].create(move_values)
+
+        return result
